d11d28d8b5d3b5a1148e138f88d4a830fc5b54b1
paoobando-netizen <pao.obando@yavirac.edu.ec>
2026-06-03 19:14:20 -0500

    Actualizar clase Producto

--- src/clases/Producto.ts
+++ src/clases/Producto.ts
@@ -1,61 +1,47 @@
-export class Persona {
-    private _nombre: string;
-    private _fechaNacimiento: Date;
-    private _estatura: number;
-    private _edad: number;
-    private _estado: boolean;
+export class Producto {
 
-    constructor(){
-        this._nombre = '';
-        this._fechaNacimiento = new Date();
-        this._estatura = 0;
-        this._edad = 0;
-        this._estado = true;
-    }
+    private _nombre: string = "";
+    private _precio: number = 0;
+    private _descuento: number = 0;
+    private _disponible: boolean = false;
 
-    public get nombre():string{
+    // GET y SET nombre
+    public get nombre(): string {
         return this._nombre;
     }
 
-    public set nombre(value:string){
+    public set nombre(value: string) {
         this._nombre = value;
     }
 
-    public get fechaNacimiento():Date{
-        return this._fechaNacimiento;
+    // GET y SET precio
+    public get precio(): number {
+        return this._precio;
     }
 
-    public set fechaNacimiento(value:Date){
-        this._fechaNacimiento = value;
+    public set precio(value: number) {
+        this._precio = value;
     }
 
-    public get estatura():number{
-        return this._estatura;
+    // GET y SET descuento
+    public get descuento(): number {
+        return this._descuento;
     }
 
-    public set estatura(value:number){
-        this._estatura = value;
+    public set descuento(value: number) {
+        this._descuento = value;
     }
 
-    public get estado():boolean{
-        return this._estado;
+    // GET y SET disponible
+    public get disponible(): boolean {
+        return this._disponible;
     }
 
-    public set estado(value:boolean){
-        this._estado = value;
+    public set disponible(value: boolean) {
+        this._disponible = value;
     }
 
-    public get edad():number{
-        const fechaActual = new Date();
-        this._edad = fechaActual.getFullYear() - this._fechaNacimiento.getFullYear();
-        return this._edad;
-    }
-
-    calcularMayorEdad():void{
-        if(this.edad >= 18){
-         console.log(`es mayor de edad`);
-        }else{
-            console.log(`es menor de edad`);
-        }
+    public calcularPrecioFinal(): number {
+        return this._precio - (this._precio * (this._descuento / 100));
     }
 }
